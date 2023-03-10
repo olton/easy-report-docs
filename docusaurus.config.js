4132308f703e2adba9fed4b505da07e39084367f
olton <serhii@pimenov.com.ua>
2023-03-10 17:53:42 +0200

    upd config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,7 +53,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/korzh-docs-social-card.png',
       navbar: {
         title: '',
         logo: {
